1f8f86f12a226b910af01e9e2417fa551c5bd385
pmhieuu <pmhieu1409@gmail.com>
2021-12-17 20:36:52 +0700

    initialize git

---
+++ Lesson-1/js/index.js
@@ -0,0 +1,16 @@
+// ES6 là một version của javascript là một cuộc đại cách mạng của JS
+// Biến số khai báo bằng let và var
+// Hằng số khai báo bằng const
+// Khi khởi tạo biến var thì JS sẽ chạy hết code sau khi chạy đến dòng var sẽ khởi tạo biến đó là undefined (đây là cơ chế hoisting của var khác với let và const)
+// let, const, var
+// scope
+// - global
+// - function: var, let, const
+// - block: let, const
+
+// DATA TYPES
+// primitives (kiểu dữ liệu nguyên thuỷ): string, number, boolen, bigint, symbol, null and undefined
+// non-primitives (kiểu dự liệu không nguyên thuỷ): object, array
+// cách kiểu tra kiểu dữ liệu dùng từ khoá typeof
+// console.log(typeof [2]); kiểu dữ liệu này là object
+// Tham trị và tham chiếu:
